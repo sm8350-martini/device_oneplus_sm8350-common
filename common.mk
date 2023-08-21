bb91e38df541bda1828a9b267b065f5012b6ec54
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    [SQUASH] sm8350-common: Introduce Dolby Atmos
    Converted existing dirac implementation based on observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    sm8450-common: Always refresh playback is status changed
    
     * otherwise dolby would stay active even if you disable its setting
    
    sm8450-common: Update dolby app package name
    
    sm8450-common: Let Dolby override MusicFX and handle EQ intent
    
    Get rid of AOSP equalizer, no one uses it. Instead advertise our
    Dolby Atmos settings as the system-wide equalizer, allowing apps
    such as Spotify to open it directly.
    
    sm8450-common: Set proper summary for Dolby settings
    
    Show the current status in Settings > Sound as well as the QS tile.
    
    sm8450-common: Introduce more Dolby Atmos effects
    
    - Bass Enhancer
    - Dialogue Enhancer
    - Stereo Widening
    
    sm8450-common: Remove play/pause hack while toggling Dolby
    
    Not required with/fixed by:
    35217: audioflinger: Do not allow DAP effect to be suspended | https://gerrit.aospa.co/c/AOSPA/android_frameworks_av/+/35217
    
    sm8450-common: Implement Dolby profile-specific settings
    
    sm8450-common: Add toggle for Dolby volume leveler
    
    sm8450-common: Checkout Dolby from POCO F5 / Redmi Note 12 Turbo
    
    sm8450-common: Re-enable speaker virtualization after bootup
    
    For whatever reason, speaker virtualization isn't automatically
    restored at bootup unlike the other parameters. It was reported to be
    fixed by connecting and disconnecting headphones or disabling and
    enabling the toggle, so let's just automate that at bootup.
    
    sm8450-common: Guard debug logging for Dolby
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I53c0597dfdf88f8d92d5ff6ec4a54df96a45aa1e

--- common.mk
+++ common.mk
@@ -163,6 +163,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+# Dolby
+PRODUCT_PACKAGES += \
+    OplusDolby
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
